2dc0837bdbbaa1ae80e1860dc9594f3d7051b332
juan <roldanjuan2003@gmail.com>
2022-10-31 15:54:13 -0300

    primer commit frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import {ProgressBarModule} from 'angular-progress-bar';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
+
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { PerfilComponent } from './perfil/perfil.component';
-import { SobremiComponent } from './sobremi/sobremi.component';
-import { EstudiosComponent } from './estudios/estudios.component';
-import { ConocimientosComponent } from './conocimientos/conocimientos.component';
-import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { SkillComponent } from './componentes/skill/skill.component';
+import { NewEduComponent } from './componentes/educacion/new-edu.component';
+import { EditEduComponent } from './componentes/educacion/edit-edu.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { NewExpComponent } from './componentes/experiencia/new-exp.component';
+import { EditExpComponent } from './componentes/experiencia/edit-exp.component';
+import { NewSkillComponent } from './componentes/skill/new-skill.component';
+import { EditSkillComponent } from './componentes/skill/edit-skill.component';
+import { EditSobremiComponent } from './componentes/sobremi/edit-sobremi.component';
+import { NewSobremiComponent } from './componentes/sobremi/new-sobremi.component';
+import { EditUserComponent } from './componentes/perfil/edit-user.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+
+
+
 
 @NgModule({
   declarations: [
@@ -16,13 +38,32 @@ import { HabilidadesComponent } from './habilidades/habilidades.component';
     HeaderComponent,
     PerfilComponent,
     SobremiComponent,
-    EstudiosComponent,
-    ConocimientosComponent,
-    HabilidadesComponent
+    EducacionComponent,
+    ExperienciaComponent,
+    SkillComponent,
+    NewEduComponent,
+    EditEduComponent,
+    HomeComponent,
+    LoginComponent,
+    NewExpComponent,
+    EditExpComponent,
+    NewSkillComponent,
+    EditSkillComponent,
+    EditSobremiComponent,
+    NewSobremiComponent,
+    EditUserComponent,
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ProgressBarModule,
+    ReactiveFormsModule,
+    AngularFireAuthModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'portfolioap-auth',
+    appId: '1:943692657807:web:414f8b9f0109454fe32cfa',
+    storageBucket: 'portfolioap-auth.appspot.com',
+    apiKey: 'AIzaSyAXRPJlHln9jWwcuyAN52-FjRbArnBcwp0',
+    authDomain: 'portfolioap-auth.firebaseapp.com',
+    messagingSenderId: '943692657807',
+  },
+  production: false,
 };
 
 /*
